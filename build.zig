7a8f70c662f91147cfdb4dd91f79118b24c109da
atolix <82761106+atolix@users.noreply.github.com>
2025-11-01 13:29:48 +0900

    project name

--- build.zig
+++ build.zig
@@ -38,7 +38,7 @@ pub fn build(b: *std.Build) void {
     // to our consumers. We must give it a name because a Zig package can expose
     // multiple modules and consumers will need to be able to specify which
     // module they want to access.
-    const mod = b.addModule("kaleid", .{
+    const mod = b.addModule("rx", .{
         // The root source file is the "entry point" of this module. Users of
         // this module will only be able to access public declarations contained
         // in this file, which means that if you have declarations that you
@@ -95,7 +95,7 @@ pub fn build(b: *std.Build) void {
     };
 
     const exe = b.addExecutable(.{
-        .name = "kaleid",
+        .name = "rx",
         .root_module = b.createModule(.{
             // b.createModule defines a new module just like b.addModule but,
             // unlike b.addModule, it does not expose the module to consumers of
@@ -110,12 +110,12 @@ pub fn build(b: *std.Build) void {
             // List of modules available for import in source files part of the
             // root module.
             .imports = &.{
-                // Here "kaleid" is the name you will use in your source code to
-                // import this module (e.g. `@import("kaleid")`). The name is
+                // Here "rx" is the name you will use in your source code to
+                // import this module (e.g. `@import("rx")`). The name is
                 // repeated because you are allowed to rename your imports, which
                 // can be extremely useful in case of collisions (which can happen
                 // importing modules from different packages).
-                .{ .name = "kaleid", .module = mod },
+                .{ .name = "rx", .module = mod },
             },
         }),
     });
